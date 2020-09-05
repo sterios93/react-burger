8641af2a0b45c778defcfc8d602963bb240a2bb1
s.taskudis <s.taskudis@dopamine.bg>
2020-09-05 19:00:47 +0300

    fix: add ingredients logic

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hocs/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+  salad: .5,
+  cheese: .4,
+  meat: 1.3,
+  bacon: .7,
+}
+
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -10,14 +17,35 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0,
+    },
+    totalPrice: 4,
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updateCount = oldCount + 1;
+    const updatedIngrediants = {
+      ...this.state.ingredients
     }
+    updatedIngrediants[type] = updateCount;
+
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+
+    this.setState({ingredients: updatedIngrediants, totalPrice: newPrice});
+  }
+
+  removeIngredientHandler = (type) => {
   }
 
   render() {
     return (
       <Aux>
           <Burger ingredients = { this.state.ingredients } />
-          <BuildControls />
+          <BuildControls
+            ingredientAdded={this.addIngredientHandler}
+          />
       </Aux>
     );
   }
