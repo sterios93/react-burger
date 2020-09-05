33042174732349c416090bafa80230b0137dca78
s.taskudis <s.taskudis@dopamine.bg>
2020-09-05 17:53:41 +0300

    feat: display call to action msg if there are no ingredients added to the burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,21 +3,27 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients =
-      Object.keys(props.ingredients)
-      .map(igKey => {
-        return [...Array(props.ingredients[igKey])].map((_, i) => {
-          return <BurgerIngredient key={igKey + i} type={igKey} />;
-        })
-      });
+  let transformedIngredients =
+    Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      })
+    })
+    .reduce((result, current) => {
+      return result.concat(current);
+    }, []);
 
-    return (
-        <div className = { classes.Burger }>
-            <BurgerIngredient type="bread-top" />
-            {transformedIngredients}
-            <BurgerIngredient type="bread-bottom" />
-        </div>
-    );
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+  return (
+      <div className = { classes.Burger }>
+          <BurgerIngredient type="bread-top" />
+          {transformedIngredients}
+          <BurgerIngredient type="bread-bottom" />
+      </div>
+  );
 }
 
 export default burger;
