ce1b1542df29720a3542b0cbab5096edc3bd2030
s.taskudis <s.taskudis@dopamine.bg>
2020-09-05 19:10:36 +0300

    fix: remove ingredient logic

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,14 +37,38 @@ class BurgerBuilder extends Component {
   }
 
   removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if (!oldCount) return;
+
+    const updateCount = oldCount - 1;
+    const updatedIngrediants = {
+      ...this.state.ingredients
+    }
+    updatedIngrediants[type] = updateCount;
+
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+
+    this.setState({ingredients: updatedIngrediants, totalPrice: newPrice});
   }
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+
     return (
       <Aux>
           <Burger ingredients = { this.state.ingredients } />
           <BuildControls
             ingredientAdded={this.addIngredientHandler}
+            ingredientRemoved={this.removeIngredientHandler}
+            disabled={disabledInfo}
           />
       </Aux>
     );
