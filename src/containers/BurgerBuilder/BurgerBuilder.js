6363e5abea167ce8497ad43543d49cf317481f70
s.taskudis <s.taskudis@dopamine.bg>
2020-09-12 18:32:02 +0300

    feat: showing & hiding the modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,6 +22,7 @@ class BurgerBuilder extends Component {
     },
     totalPrice: 4,
     purchasable: false,
+    purchasing: false,
   }
 
   updatePurchaseState = (ingredients) => {
@@ -70,6 +71,10 @@ class BurgerBuilder extends Component {
     this.updatePurchaseState(updatedIngrediants);
   }
 
+  purchaseHandler = () => {
+    this.setState({purchasing: true});
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -81,7 +86,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-          <Modal>
+          <Modal show={this.state.purchasing}>
             <OrderSummary ingredients={this.state.ingredients}/>
           </Modal>
           <Burger ingredients = { this.state.ingredients } />
@@ -90,7 +95,9 @@ class BurgerBuilder extends Component {
             ingredientRemoved={this.removeIngredientHandler}
             disabled={disabledInfo}
             purchasable={this.state.purchasable}
-            price={this.state.totalPrice}/>
+            price={this.state.totalPrice}
+            ordered={this.purchaseHandler}
+            />
       </Aux>
     );
   }
